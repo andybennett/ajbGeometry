77e76aae87135fb7152c92a90cfa764ad3a44956
andyj <andyj@AndysLaptop.home>
2019-06-02 13:36:39 +0100

    Continued Work

--- src/ajb/geometry/GeometryUtils.java
+++ src/ajb/geometry/GeometryUtils.java
@@ -55,8 +55,7 @@ public class GeometryUtils {
 
 		Point2D.Double point = GeometryUtils.findRandomPointWithinArea(area);
 
-		area.add(new Area(new Rectangle2D.Double(point.x, point.y, RandomInt.anyRandomIntRange(1, 10),
-				RandomInt.anyRandomIntRange(10, 30))));
+		addBlockAtPoint(area, point);
 
 	}
 	
